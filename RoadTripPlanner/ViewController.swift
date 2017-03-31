3deac7f6b884c49b2a2e1562fddef84834958f3a
safath <safath505@gmail.com>
2017-03-31 08:49:00 -0500

    Initial Commit

---
+++ RoadTripPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoadTripPlanner
+//
+//  Created by Safath Zaman on 3/31/17.
+//  Copyright © 2017 Safath Zaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
